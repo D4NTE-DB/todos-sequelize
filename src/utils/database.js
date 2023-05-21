dbad900aaf08304dd95b8eea876d7b7b4a1c13c3
D4NTE-DB <dantebabilonia98@gmail.com>
2023-05-21 15:27:21 -0500

    s

--- src/utils/database.js
+++ src/utils/database.js
@@ -1,13 +1,22 @@
 const { Sequelize } = require('sequelize')
 
+require("dotenv").config();
 
 const db = new Sequelize({
-    database: 'sequelizetodos',
-    port: '5432',
-    host: 'localhost',  
-    username: 'postgres',
-    password: 'root',
-    dialect: "postgres"
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    host: process.env.DB_HOST,  
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    dialect: "postgres",
+    dialectOptions: { ssl: { require: true, rejectUnauthorized: false } }
 });
 
 module.exports = db;
+
+// database: 'sequelizetodos',
+// port: '5432',
+// host: 'localhost',  
+// username: 'postgres',
+// password: 'root',
+// dialect: "postgres"
